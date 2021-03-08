294d90c81b5b08f0eb93aafa2e0062da50c741ce
Ejdzyn <adrian.wstepnik@gmail.com>
2021-03-08 16:37:54 +0100

    navbar.js impl

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+let nav = '<nav><li><a href="index.html">Home</a></li>' + '<li><a href="contact.html">Kontakt</a></li></nav>';
+
+let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wyżej
+
+navbarElem.innerHTML = nav;
+
