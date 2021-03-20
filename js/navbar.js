ab5d7b91bced1a28417b0c11a7ba3f74ee0e6594
Ejdzyn <adrian.wstepnik@gmail.com>
2021-03-20 18:12:23 +0100

    quiz init

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,8 @@
-let nav = '<nav><li><a href="index.html">Home</a></li>' + '<li><a href="contact.html">Kontakt</a></li></nav>';
+let nav = '<nav>' +
+    '<li><a href="index.html">Home</a></li>' +
+    '<li><a href="contact.html">Kontakt</a></li>' +
+    '<li><a href="quiz.html">Quiz</a></li>' +
+    '</nav>';
 
 let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wyżej
 
